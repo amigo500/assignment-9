24f1ef7c4957125fea1dc0422aeed4ef95280a5f
Alexander Levin <95171959+amigo500@users.noreply.github.com>
2022-01-17 04:40:13 +0100

    Add files via upload

---
+++ 9.3 -todolist_shopping/js/to do list.js
@@ -0,0 +1,73 @@
+// remove all button
+
+const button = document.createElement('button')
+button.innerText = 'REMOVE ALL ITEMS'
+button.id = 'removebutton'
+button.addEventListener('click', () => {removeAll()})
+document.body.appendChild(button)
+
+
+
+
+
+// close cross button to remove it from list
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// clicking close button to hide the item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+  var div = this.parentElement;
+  div.style.display = "none";
+  }
+}
+
+// symbol when clicking on item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+  ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Produce a new item when you click on the "Put Item into List" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+  alert("Nada input, please enter your item!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+  }
+}
+
+//Remove all items
+function removeAll(){
+  var lst = document.getElementsByTagName("ul");
+    lst[0].innerHTML = "";
+}
